9135c441ef40bffa9f1b45e93b6a2c273861716c
Ilya Petrov <dnamix1@gmail.com>
2019-12-11 16:49:45 +0300

    added food sections

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,42 +1,61 @@
-export const foods = [
+export const foodItems = [
     {
         name: "Cheese Pizza",
-        img: '/img/pizza.png'
+        img: '/img/pizza.png',
+        section: "Pizza",
     },
     {
         name: "Pepperoni Pizza",
-        img: '/img/pizza2.jpeg'
+        img: '/img/pizza2.jpeg',
+        section: "Pizza",
     },
     {
         name: "Burger",
-        img: '/img/burger.jpeg'
+        img: '/img/burger.jpeg',
+        section: "Sandwich"
     },
     {
         name: "Fries",
-        img: '/img/fries.jpeg'
+        img: '/img/fries.jpeg',
+        section: "Sides"
     },
     {
         name: "Veggie Pizza",
-        img: '/img/healthy-pizza.jpeg'
+        img: '/img/healthy-pizza.jpeg',
+        section: "Pizza"
     },
     {
         name: "Chicken Pizza",
-        img: '/img/chicken-pizza.jpeg'
+        img: '/img/chicken-pizza.jpeg',
+        section: "Pizza"
     },
     {
         name: "Club Sandwich",
-        img: '/img/sandwich.jpeg'
+        img: '/img/sandwich.jpeg',
+        section: "Sandwich"
     },
     {
         name: "Gyro",
-        img: '/img/gyro.jpeg'
+        img: '/img/gyro.jpeg',
+        section: "Sandwich"
     },
     {
         name: "Chicken Fingers",
-        img: '/img/chicken-fingers.jpeg'
+        img: '/img/chicken-fingers.jpeg',
+        section: "Sides"
     },
     {
         name: "Shrimp Sandwich",
-        img: '/img/shrimp.jpg'
+        img: '/img/shrimp.jpg',
+        section: "Sandwich"
     }
 ];
+
+export const foods = foodItems.reduce((res, food) => {
+ if (!res[food.section]){
+     res[food.section] = []
+ }
+ res[food.section].push(food);
+ return res;
+}, {});
+
